122914f5095b720af8783667ed7a0df8ce0d1aeb
prateekatknoldus <prateek.gupta@knoldus.com>
2021-03-22 14:14:28 +0530

    Modified the test cases in UserServiceSpec class

--- src/test/scala/org/knoldus/service/UserServiceSpec.scala
+++ src/test/scala/org/knoldus/service/UserServiceSpec.scala
@@ -4,31 +4,30 @@ import org.knoldus.model.{Type, User}
 import org.knoldus.repository.UserRepository
 import org.mockito.MockitoSugar.{mock, when}
 import org.scalatest.flatspec.AnyFlatSpec
-
+import org.scalatest.matchers.should.Matchers
 import java.util.UUID
+import org.scalatest.OptionValues
 
 // class having Unit Test Cases for UserService class
-class UserServiceSpec extends AnyFlatSpec{
+class UserServiceSpec extends AnyFlatSpec with Matchers with OptionValues {
+  val mockedUserRepository = mock[UserRepository]
+  val userService = new UserService(mockedUserRepository)
 
   // Unit Test Cases for createUser method
   behavior of "createUser method"
 
   it should "create a user and returns its ID if the ID is not provided by the user itself" in{
-    val mockedUserRepository = mock[UserRepository]
     val newUser:User = User(None,"prateek", "12345", Type.Customer)
     when(mockedUserRepository.create(newUser)) thenReturn Some(UUID.randomUUID())
 
-    val userService = new UserService(mockedUserRepository)
     val newUserId = userService.createUser(newUser)
     assert(newUserId.isDefined)
   }
 
   it should "not create a user if the ID has been provided by the user itself " in{
-    val mockedUserRepository = mock[UserRepository]
     val newUser:User = User(Some(UUID.randomUUID()),"shubham", "54321", Type.Admin)
     when(mockedUserRepository.create(newUser)) thenReturn None
 
-    val userService = new UserService(mockedUserRepository)
     val newUserId = userService.createUser(newUser)
     assert(newUserId.isEmpty)
   }
@@ -37,22 +36,15 @@ class UserServiceSpec extends AnyFlatSpec{
   behavior of "getUserByID method"
 
   it should "return the user if the given user ID exists" in{
-    val mockedUserRepository = mock[UserRepository]
     val newUser = User(None,"prateek", "12345", Type.Customer)
-    when(mockedUserRepository.create(newUser)) thenReturn Some(UUID.randomUUID())
+    val newUserId = UUID.randomUUID()
+    when(mockedUserRepository.getByID(Some(newUserId))) thenReturn newUser.copy(id = Some(newUserId))
 
-    val userService = new UserService(mockedUserRepository)
-    val newUserID = userService.createUser(newUser)
-
-    when(mockedUserRepository.getByID(newUserID)) thenReturn newUser.copy(id = newUserID)
-
-    val retrievedUser = userService.getUserByID(newUserID)
-    assert(retrievedUser.id == newUserID)
+    val retrievedUser = userService.getUserByID(Some(newUserId))
+    retrievedUser.id should contain(newUserId)
   }
 
   it should "throw NoSuchElementException if the given user ID does not exists" in{
-    val mockedUserRepository = mock[UserRepository]
-    val userService = new UserService(mockedUserRepository)
     val randomId = Some(UUID.randomUUID())
     when(mockedUserRepository.getByID(randomId)) thenThrow new NoSuchElementException()
 
@@ -65,23 +57,16 @@ class UserServiceSpec extends AnyFlatSpec{
   behavior of "updateUser method"
 
   it should "return true if it successfully updated a user corresponding to the given user id" in{
-    val mockedUserRepository = mock[UserRepository]
-    val newUser = User(None,"prateek", "12345", Type.Customer)
-    when(mockedUserRepository.create(newUser)) thenReturn Some(UUID.randomUUID())
-    val userService = new UserService(mockedUserRepository)
-    val newUserID = userService.createUser(newUser)
-    when(mockedUserRepository.getByID(newUserID)) thenReturn newUser.copy(id = newUserID)
+    val userId = Some(UUID.randomUUID())
+    val updatedUser = User(Some(UUID.randomUUID()),"Rahul", "12345", Type.Customer)
 
-    val updatedUser = userService.getUserByID(newUserID).copy(username="chaitanya")
-    when(mockedUserRepository.update(newUserID,updatedUser)) thenReturn true
+    when(mockedUserRepository.update(userId,updatedUser)) thenReturn true
 
-    val isUserUpdated = userService.updateUser(newUserID,updatedUser)
+    val isUserUpdated = userService.updateUser(userId,updatedUser)
     assert(isUserUpdated)
   }
 
   it should "throw a NoSuchElementException if the given user ID does not exists" in{
-    val mockedUserRepository = mock[UserRepository]
-    val userService = new UserService(mockedUserRepository)
     val randomId = Some(UUID.randomUUID())
     val updatedUser = User(Some(UUID.randomUUID()),"shubham", "54321", Type.Admin)
     when(mockedUserRepository.update(randomId,updatedUser)) thenThrow new NoSuchElementException()
@@ -95,21 +80,14 @@ class UserServiceSpec extends AnyFlatSpec{
   behavior of "deleteUser method"
 
   it should "return true if it successfully deleted a user corresponding to the given user id" in{
-    val mockedUserRepository = mock[UserRepository]
-    val newUser = User(None,"prateek", "12345", Type.Customer)
-    when(mockedUserRepository.create(newUser)) thenReturn Some(UUID.randomUUID())
-    val userService = new UserService(mockedUserRepository)
-    val newUserID = userService.createUser(newUser)
+    val userId = Some(UUID.randomUUID())
+    when(mockedUserRepository.delete(userId)) thenReturn true
 
-    when(mockedUserRepository.delete(newUserID)) thenReturn true
-
-    val isUserDeleted = userService.deleteUser(newUserID)
+    val isUserDeleted = userService.deleteUser(userId)
     assert(isUserDeleted)
   }
 
   it should "throw a NoSuchElementException if the given user ID does not exists" in{
-    val mockedUserRepository = mock[UserRepository]
-    val userService = new UserService(mockedUserRepository)
     val randomId = Some(UUID.randomUUID())
     when(mockedUserRepository.delete(randomId)) thenThrow new NoSuchElementException()
 
@@ -122,18 +100,12 @@ class UserServiceSpec extends AnyFlatSpec{
   behavior of "getAllUsers method"
 
   it should "return a list of all the users" in{
-    val mockedUserRepository = mock[UserRepository]
-    val userService = new UserService(mockedUserRepository)
     when(mockedUserRepository.getAll) thenReturn List(User(Some(UUID.randomUUID()),"shubham", "54321", Type.Admin),User(Some(UUID.randomUUID()),"prateek", "3021", Type.Customer))
-
     assert(userService.getAllUsers.nonEmpty)
   }
 
   it should "return an empty list if no users exits in database" in{
-    val mockedUserRepository = mock[UserRepository]
-    val userService = new UserService(mockedUserRepository)
     when(mockedUserRepository.getAll) thenReturn List.empty
-
     assert(userService.getAllUsers.isEmpty)
   }
 
